00ccc561b488108f86a3dc79fb16164d1ed62936
Bhoomikarwani <bhoomikarwani@gmail.com>
2026-01-01 21:30:34 +0530

    Task 4

--- Task4/script.js
+++ Task4/script.js
@@ -9,21 +9,31 @@ listItems.forEach(item =>{
     item.addEventListener("click", ()=>{
         item.classList.toggle('completed');
   
-    if(item.classList.contains('completed')){
+       if(item.classList.contains('completed')){
         count++;
-    }
-    else{
+       }
+       else{
         count--;
-    }
+       }
     counter.innerText=count;
 
     const percentage=(count/totalElements)*100;
     progressBar.style.width=percentage + "%";
-
-     unmarkAll.addEventListener("click", ()=>{
-       if(listItems.classList.contains('completed')){
-        
-       }
-     });
+    });
+});
+markAll.addEventListener("click",()=>{
+    listItems.forEach(val =>{
+        val.classList.add("completed");
+        count=5;
+        counter.innerText=count;
+        progressBar.style.width= 100+"%";
+    });
+});
+unmarkAll.addEventListener("click",()=>{
+    listItems.forEach(val =>{
+        val.classList.remove("completed");
+        count=0;
+        counter.innerText=count;
+        progressBar.style.width= 0+"%";
     });
 });
